61b02d6da8da3f5c0fa211be6fa6047b245e74e5
Ed Fuh <ed@storenvy.com>
2012-01-25 19:55:37 -0800

    add AMD support, and Node.js support (needs tests)

--- source/core.js
+++ source/core.js
@@ -1,36 +1,54 @@
-var Liquid = {
-
-  author: '<%= AUTHOR %>',
-  version: '<%= VERSION %>',
-
-  readTemplateFile: function(path) {
-    throw ("This liquid context does not allow includes.");
-  },
-  
-  registerFilters: function(filters) {
-    Liquid.Template.registerFilter(filters);
-  },
-  
-  parse: function(src) {
-    return Liquid.Template.parse(src);
+(function(root, factory) {
+  // Set up Liquid appropriately for the environment.
+  if (typeof exports !== 'undefined') {
+    // Node/CommonJS
+    exports.Liquid = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define('liquid', [], factory);
+  } else {
+    // Browser globals
+    root.Liquid = factory();
   }
+}(this, function () {
+
+  var Liquid = {
+
+    author: '<%= AUTHOR %>',
+    version: '<%= VERSION %>',
+
+    readTemplateFile: function(path) {
+      throw ("This liquid context does not allow includes.");
+    },
   
-};
+    registerFilters: function(filters) {
+      Liquid.Template.registerFilter(filters);
+    },
+  
+    parse: function(src) {
+      return Liquid.Template.parse(src);
+    }
+  
+  };
 
-//= require "extensions"
-//= require "class"
-//= require "tag"
-//= require "block"
-//= require "document"
-//= require "strainer"
-//= require "context"
-//= require "template"
-//= require "variable"
-//= require "condition"
-//= require "drop"
-//= require "default_tags"
-//= require "default_filters"
+  //= require "extensions"
+  //= require "class"
+  //= require "tag"
+  //= require "block"
+  //= require "document"
+  //= require "strainer"
+  //= require "context"
+  //= require "template"
+  //= require "variable"
+  //= require "condition"
+  //= require "drop"
+  //= require "default_tags"
+  //= require "default_filters"
 
 
-//= require <strftime>
-//= require <split>
+  //= require <strftime>
+  //= require <split>
+
+  return Liquid;
+
+}));
