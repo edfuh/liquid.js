ad85d6955b26abceafdefdd382988d5b9d660433
Ed Fuh <ed@storenvy.com>
2012-03-14 12:23:35 -0700

    extension improvements

--- source/extensions.js
+++ source/extensions.js
@@ -1,34 +1,56 @@
 // Array.indexOf
+// not to spec, but super fast
 if (!Array.prototype.indexOf) {
-  Array.prototype.indexOf = function(obj) {
-    for (var i=0; i<this.length; i++) {
-      if (this[i] == obj) return i;
-    }
+  Array.prototype.indexOf = function (searchElement) {
+    "use strict";
+    var
+      t = this,
+      length = t.length,
+      i = 0
+    ;
 
+    while (i < length) {
+      if (t[i] === searchElement) {
+        return i;
+      }
+      i++;
+    }
     return -1;
+  };
+}
+
+// Array.include
+if (!Array.prototype.include) {
+  Array.prototype.include = function (arg) {
+    return !!~this.indexOf(arg);
   };
 }
 
 // Array.clear
 if (!Array.prototype.clear) {
-  Array.prototype.clear = function() {
-    //while (this.length > 0) this.pop();
+  Array.prototype.clear = function () {
     this.length = 0;
   };
 }
 
 // Array.map
 if (!Array.prototype.map) {
-  Array.prototype.map = function(fun /*, thisp*/) {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw 'Array.map requires first argument to be a function';
+  Array.prototype.map = function (fun, context) {
+    "use strict";
+    if (typeof fun !== 'function') {
+      throw new TypeError(fun + ' is not a function');
+    }
 
-    var res = new Array(len);
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++) {
-      if (i in this)
+    var
+      len = this.length,
+      res = new Array(len),
+      thisp = context || this,
+      i
+    ;
+    for (i = 0; i < len; i++) {
+      if (typeof this[i] !== 'undefined') {
         res[i] = fun.call(thisp, this[i], i, this);
+      }
     }
 
     return res;
@@ -37,24 +59,28 @@ if (!Array.prototype.map) {
 
 // Array.first
 if (!Array.prototype.first) {
-  Array.prototype.first = function() {
+  Array.prototype.first = function () {
     return this[0];
   };
 }
 
 // Array.last
 if (!Array.prototype.last) {
-  Array.prototype.last = function() {
+  Array.prototype.last = function () {
     return this[this.length - 1];
   };
 }
 
 // Array.flatten
 if (!Array.prototype.flatten) {
-  Array.prototype.flatten = function() {
-    var len = this.length;
-    var arr = [];
-    for (var i = 0; i < len; i++) {
+  Array.prototype.flatten = function () {
+    "use strict";
+    var
+      len = this.length,
+      arr = [],
+      i
+    ;
+    for (i = 0; i < len; i++) {
       // TODO This supposedly isn't safe in multiple frames;
       // http://stackoverflow.com/questions/767486/how-do-you-check-if-a-variable-is-an-array-in-javascript
       // http://stackoverflow.com/questions/4775722/javascript-check-if-object-is-array
@@ -71,45 +97,37 @@ if (!Array.prototype.flatten) {
 
 // Array.forEach
 if (!Array.prototype.forEach) {
-  Array.prototype.forEach = function(fun /*, thisp*/) {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw 'Array.each requires first argument to be a function';
+  Array.prototype.forEach = function (fun, context) {
+    "use strict";
+    if (typeof fun !== 'function') {
+      throw new TypeError(fun + ' is not a function');
+    }
 
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++) {
-      if (i in this)
+    var
+      len = this.length,
+      thisp = context || this,
+      i
+    ;
+    for (i = 0; i < len; i++) {
+      if (typeof this[i] !== 'undefined') {
         fun.call(thisp, this[i], i, this);
+      }
     }
 
     return null;
   };
 }
 
-// Array.include
-if (!Array.prototype.include) {
-  Array.prototype.include = function(arg) {
-    var len = this.length;
-
-    return this.indexOf(arg) >= 0;
-    for (var i = 0; i < len; i++) {
-      if (arg == this[i]) return true;
-    }
-
-    return false;
-  };
-}
-
 // String.capitalize
 if (!String.prototype.capitalize) {
-  String.prototype.capitalize = function() {
+  String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.substring(1).toLowerCase();
   };
 }
 
 if (!String.prototype.trim) {
   String.prototype.trim = function () {
-    return this.replace(/^\s+|\s+$/g,'');
+    return this.replace(/^\s+|\s+$/g, '');
   };
 }
 
@@ -121,8 +139,9 @@ Liquid.extensions = {};
 Liquid.extensions.object = {};
 
 // Object.update
-Liquid.extensions.object.update = function(newObj) {
-  for (var p in newObj) {
+Liquid.extensions.object.update = function (newObj) {
+  var p;
+  for (p in newObj) {
     this[p] = newObj[p];
   }
 
@@ -133,7 +152,7 @@ Liquid.extensions.object.update = function(newObj) {
 //}
 
 // Object.hasKey
-Liquid.extensions.object.hasKey = function(arg) {
+Liquid.extensions.object.hasKey = function (arg) {
   return !!this[arg];
 };
 //if (!Object.prototype.hasKey) {
@@ -141,9 +160,12 @@ Liquid.extensions.object.hasKey = function(arg) {
 //}
 
 // Object.hasValue
-Liquid.extensions.object.hasValue = function(arg) {
-  for (var p in this) {
-    if (this[p] == arg) return true;
+Liquid.extensions.object.hasValue = function (arg) {
+  var p;
+  for (p in this) {
+    if (this[p] == arg) {
+      return true;
+    }
   }
 
   return false;
